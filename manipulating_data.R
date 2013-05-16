830b6a8117fbfd03238dddaee2ecd55f590b36d3
bcgalvin <bcgalvin@gmail.com>
2013-05-16 12:33:54 -0700

    basic data manipulation

---
+++ manipulating_data.R
@@ -0,0 +1,59 @@
+#########################
+#manipulating data frames
+#########################
+
+titanic <- read.csv(file='titanic.csv', as.is=T,header=T)
+
+                                                    
+#dataframe subscripting: [row,column]
+
+#subscripting + c():
+titanic_lite <- titanic[,-c(3,8,10)]
+
+#names() + %in%
+titanic_lite2 <- titanic[,names(titanic) %in% c("survived","pclass","sex","age","sibsp","parch","fare","embarked")]
+
+#sqldf package
+library(sqldf)
+?sqldf
+titanic_lite3 <- sqldf("select survived, pclass, sex, age, sibsp, parch, fare, embarked from titanic")
+
+
+
+# $ operator
+titanic$survived #entire vector of the survived column
+titanic$survived[1:10] #first 10 rows of the survived column
+
+
+#Subsetting with logicals
+titanic_sub <- titanic[titanic$sex == 'male',] #tells R that you want all rows that have a male passenger and all columns
+titanic_sub$sex #all males
+
+#with subset()
+?subset
+titanic_sub <- subset(titanic, titanic$sex=='male')
+
+
+#creating new variables
+?hist
+hist(titanic$fare) # histogram
+titanic$fare_log <- log(titanic$fare)
+
+hist(titanic$fare_log)
+
+
+#recoding variables
+titanic$sex[titanic$sex=='male'] <- 'm'
+titanic$sex[titanic$sex=='female'] <-'f'
+
+library(car)
+titanic$sex <- recode(titanic$sex, "'m'='male';else='female'")
+
+
+#renaming variables
+names(titanic)
+
+names(titanic) <- c("surv", "class", "name","sex", "age", "sib",
+                    "par", "ticket","fare", "cabin", "embarked")
+names(titanic)
+
