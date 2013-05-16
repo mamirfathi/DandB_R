af256797a2070be5bb832e18e87e4e93ea6ee83c
bcgalvin <bcgalvin@gmail.com>
2013-05-16 15:30:30 -0700

    Basic data manipulation

--- manipulating_data.R
+++ manipulating_data.R
@@ -5,8 +5,6 @@
 titanic <- read.csv(file='titanic.csv', as.is=T,header=T)
 
                                                     
-#dataframe subscripting: [row,column]
-
 #subscripting + c():
 titanic_lite <- titanic[,-c(3,8,10)]
 
@@ -19,17 +17,17 @@ library(sqldf)
 titanic_lite3 <- sqldf("select survived, pclass, sex, age, sibsp, parch, fare, embarked from titanic")
 
 
-
 # $ operator
 titanic$survived #entire vector of the survived column
 titanic$survived[1:10] #first 10 rows of the survived column
 
 
-#Subsetting with logicals
+#subsetting
+          #with logicals
 titanic_sub <- titanic[titanic$sex == 'male',] #tells R that you want all rows that have a male passenger and all columns
 titanic_sub$sex #all males
 
-#with subset()
+          #with subset()
 ?subset
 titanic_sub <- subset(titanic, titanic$sex=='male')
 
@@ -43,9 +41,11 @@ hist(titanic$fare_log)
 
 
 #recoding variables
+          #with logicals
 titanic$sex[titanic$sex=='male'] <- 'm'
 titanic$sex[titanic$sex=='female'] <-'f'
 
+          #with recode
 library(car)
 titanic$sex <- recode(titanic$sex, "'m'='male';else='female'")
 
@@ -57,7 +57,9 @@ names(titanic) <- c("surv", "class", "name","sex", "age", "sib",
                     "par", "ticket","fare", "cabin", "embarked")
 names(titanic)
 
+
 #combining R objects
+?rbind
           #row bind:
 titanic_male <- titanic[titanic$sex=='male',]
 titanic_female <- titanic[titanic$sex != 'male',]
@@ -71,3 +73,13 @@ names(titanic_lite)
 names(titanic_lite2)
 
 titanic_combined2 <- cbind(titanic_lite,titanic_lite2)
+
+
+#ordering R objects
+?sort
+fare_sort <- sort(titanic$fare, decreasing=TRUE) #vector
+head(fare_sort)
+
+?order
+titanic_sort <- titanic[order(titanic$fare, decreasing=TRUE),]
+head(titanic_sort)
