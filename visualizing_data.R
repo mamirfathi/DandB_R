0bb9e9bf0ceb95a7415e9afef92eb9a68a03ab1d
bcgalvin <bcgalvin@gmail.com>
2013-05-28 10:14:51 -0700

    ggplot2 stuff added

--- visualizing_data.R
+++ visualizing_data.R
@@ -74,5 +74,61 @@ boxplot(titanic$age,
         col='dodgerblue')
 
 plot(titanic$age, titanic$fare)
+            
+
+#histogram:
+par(mfrow=c(1,1)) #reset plot window
+hist(titanic$age, col="dodgerblue", main="Age Histogram", xlab="Age")
+
+#density plot
+df <- density(titanic$age[which(titanic$sex=='female')],na.rm=T)
+dm <- density(titanic$age[which(titanic$sex=='male')],na.rm=T)
+
+plot(dm,col="dodgerblue",lwd=2, main="Age Density", xlab="Age")
+lines(df,col="orange",lwd=2)
+legend("topright", c("male","female"), lty=c(1,1), lwd=c(2,2),col=c("dodgerblue","orange") )
+
+#scatter plot
+data(mtcars)
+plot(mtcars$hp, mtcars$mpg, main="MPG/HP Scatterplot",
+     xlab="Car Horsepower ", ylab="Miles Per Gallon ", pch=19) 
+
+#scatter matrix
+pairs(~mpg+disp+drat+wt,data=mtcars,
+      main="Simple Scatterplot Matrix")
+
 
 #ggplot2
+?ggplot          
+ex <- ggplot(data=titanic)
+
+#histogram
+ex + geom_histogram(aes(x=fare,fill=as.factor(sex)),binwidth=25)
+
+#boxplot
+ex + geom_boxplot(aes(x=as.factor(pclass),y=fare))
+
+#barplot
+ex  <- ggplot(data=titanic, aes(x=as.factor(pclass)))
+
+ex+geom_bar()+labs(title='Standard Bar Graph')  #standard bar
+
+ex+geom_bar(aes(fill=sex))+labs(title='Filled by sex') #put colors in by another variables
+
+ex+geom_bar(aes(fill=sex),position='fill')+labs(title='A stacked Ratio using "position=fill"') # stacked ratio
+
+ex+geom_bar(aes(fill=sex),position='dodge')+labs(title='Side by Side, using "position=dodge"') #put them side by side
+
+#scales + facets
+ex<-ggplot(data=titanic,aes(x=age,y=fare))
+ex1<-ex+geom_point(aes(shape=as.factor(pclass)))+labs(title='Different Shapes for different classes')
+ex1
+
+ex2<-ex+geom_point(alpha=0.25,aes(shape=as.factor(pclass)))+labs(title='Different Shapes with Transparency')
+ex2
+
+ex3<-ex1+facet_grid(survived~sex)+labs(title='survived/sex facet grid')
+ex3
+
+ex4<-ex3+geom_smooth(method='lm',size=1)+labs(title='regression line for each facet')
+ex4
